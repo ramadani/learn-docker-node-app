3970a9ce6c98bc8488c096ffc5fd302b0bd12d5a
Ramadani <email.ramadani@gmail.com>
2018-06-09 13:58:00 +0800

    get hostname

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const os = require('os');
+
 const app = express();
 const port = process.env.PORT;
 const env = process.env.NODE_ENV;
+const hostname = os.hostname();
 
-app.get('/', (req, res) => res.send(`Hello Example App in ${env}!`));
+app.get('/', (req, res) => res.send(`Hello Example App in ${env} at ${hostname}!`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
