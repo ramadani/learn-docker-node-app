5f8f3f70e8bdd61f555db29291b2777ca8f2fbfe
Ramadani <email.ramadani@gmail.com>
2018-06-09 13:26:35 +0800

    docker service env

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn-docker-node-app
 How to Dockerizing a Node.js Web App
+
+- [ ] environment variable
+- [x] nodemon
+- [ ] mysql
--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const env = process.env.NODE_ENV;
 
-app.get('/', (req, res) => res.send('Hello Example App!'));
+app.get('/', (req, res) => res.send(`Hello Example App in ${env}!`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
